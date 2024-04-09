bdf7fbeb215664261f33db01e9200aa6ff4e6a4d
Allufs <allu.games00@gmail.com>
2024-04-09 08:56:23 +0300

    Tallennus 9.4 aamu

--- EnemyScripts/enemyHealth_2.0.cs
+++ EnemyScripts/enemyHealth_2.0.cs
@@ -11,26 +11,15 @@ public class EnemyHealth : MonoBehaviour
     //lisä
     private Rigidbody2D rb;
 
-    //lisä
-
-    //[SerializeField] StatusBarHealth statusBar;
-
-    private void Awake()
-    {
-       // statusBar = GetComponent<StatusBarHealth>();
-    }
-
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
-        //statusBar.UpdateStatusBar(health, maxHealth);
     }
 
     // Update is called once per frame
     void Update()
    {
-        // statusBar.UpdateStatusBar(health, maxHealth);
         if (health <= 0)
         {
             Destroy(gameObject);
---
+++ Perseilytä/Assets/Scripts/EnemyScripts/enemyHealth_2.0.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float health;
+    public float maxHealth = 100f;
+
+    //lisä
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+   {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+   
+    
+
+}
